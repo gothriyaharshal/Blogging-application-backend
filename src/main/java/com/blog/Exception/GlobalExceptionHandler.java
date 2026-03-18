ddbc058adcb50e88fecc03b6959208b4b69cb600
Harshal gothriya <114577770+gothriyaharshal@users.noreply.github.com>
2026-03-18 21:37:13 +0530

    Update GlobalExceptionHandler.java

--- src/main/java/com/blog/Exception/GlobalExceptionHandler.java
+++ src/main/java/com/blog/Exception/GlobalExceptionHandler.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.Map;
 
-@RestController
+@RestControllerAdvice
 
 public class GlobalExceptionHandler {
 
